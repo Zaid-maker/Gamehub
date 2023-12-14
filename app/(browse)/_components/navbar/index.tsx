b101fe02052477c53622bc5aff34e990144d771d
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-14 19:19:13 +0000

    update and include Logo component

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Logo } from "./logo";
 
 export const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
+      <Logo />
+    </nav>
+  );
 };
