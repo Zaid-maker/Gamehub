2ec3fcfb50dcfa4ba78f7d02dd63f297edd962d6
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-14 19:14:16 +0000

    create Navbar component

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Navbar } from "./_components/navbar";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Navbar />
       <div className="flex h-full pt-20">{children}</div>
     </>
   );
