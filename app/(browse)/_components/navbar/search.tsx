12530f95f0420d8125f8abd57b40c4b54450bcf0
DevMirza <pzhafeez@gmail.com>
2023-12-15 18:40:17 +0000

    update the search component

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,7 +1,51 @@
 "use client";
 
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import React, { useState } from "react";
 
 export const Search = () => {
-  return <div>Search</div>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: { term: value },
+      },
+      { skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="relative w-full lg:w-[400px] flex items-center"
+    >
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search"
+        className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+      />
+      <Button
+        type="submit"
+        size="sm"
+        variant="secondary"
+        className="rounded-l-none"
+      >
+        <SearchIcon className="h-5 w-5 text-muted-foreground" />
+      </Button>
+    </form>
+  );
 };
