d63049466c1684757e75e40e3b121c132ca6fa47
DevMirza <pzhafeez@gmail.com>
2023-12-15 18:45:02 +0000

    add onClear and Icon

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import qs from "query-string";
 import React, { useState } from "react";
@@ -18,13 +18,17 @@ export const Search = () => {
 
     const url = qs.stringifyUrl(
       {
-        url: "/",
+        url: "/search",
         query: { term: value },
       },
       { skipEmptyString: true }
     );
 
     router.push(url);
+  };
+
+  const onClear = () => {
+    setValue("");
   };
 
   return (
@@ -38,6 +42,12 @@ export const Search = () => {
         placeholder="Search"
         className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
       />
+      {value && (
+        <X
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+          onClick={onClear}
+        />
+      )}
       <Button
         type="submit"
         size="sm"
