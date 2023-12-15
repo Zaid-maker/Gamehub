9f83cdb677c1fc3c5bbdce5ed58695115999b294
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-15 19:35:16 +0000

    add styling to sidebar

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -5,5 +5,9 @@ interface WrapperProps {
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
-  return <aside>{children}</aside>;
+  return (
+    <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      {children}
+    </aside>
+  );
 };
