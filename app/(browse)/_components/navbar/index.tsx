9ceea9618c338be343b558d2968c2e0d37ca9c50
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-15 18:15:49 +0000

    create Seach component

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Logo } from "./logo";
+import { Search } from "./search";
 
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
       <Logo />
+      <Search />
     </nav>
   );
 };
