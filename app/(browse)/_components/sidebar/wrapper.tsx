93c4f5643d1605f25063c9bea25c550017e566d1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-15 19:46:53 +0000

    update Wrapper component

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
 import React from "react";
 
 interface WrapperProps {
@@ -5,8 +9,15 @@ interface WrapperProps {
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const { collapsed } = useSidebar((state) => state);
+
   return (
-    <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
       {children}
     </aside>
   );
