757f2af341653da0d6c24f60d833634a5b626a94
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-15 19:24:49 +0000

    create Sidebar component

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        {children}
+      </div>
     </>
   );
 };
