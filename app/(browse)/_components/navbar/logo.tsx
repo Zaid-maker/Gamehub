27c8eae8c53da9984b0582b7f92c19adc9b62e74
DevMirza <pzhafeez@gmail.com>
2023-12-15 19:10:46 +0000

    show logo on mobile view

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -11,11 +11,11 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1">
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
           <Image src="/spooky.svg" alt="gamehub" height="32" width="32" />
         </div>
-        <div className={cn(font.className)}>
+        <div className={cn(font.className, "hidden lg:block")}>
           <p className="text-lg font-semibold">Gamehub</p>
           <p className="text-xs text-muted-foreground">Let&apos;s play</p>
         </div>
