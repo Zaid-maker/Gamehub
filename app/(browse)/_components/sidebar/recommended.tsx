411f17adc95df4813cff93ae023cdd958d7cffe9
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-16 14:14:31 +0000

    fix

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -2,7 +2,7 @@ import { User } from "@prisma/client";
 import React from "react";
 
 interface RecommendedProps {
-  data: User;
+  data: User[];
 }
 
 export const Recommended = ({ data }: RecommendedProps) => {
