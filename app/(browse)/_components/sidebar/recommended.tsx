41ce463e63c25e52c6d6771c9f7d4c61cd74fe3c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-16 14:21:55 +0000

    update recommended component

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { useSidebar } from "@/store/use-sidebar";
 import { User } from "@prisma/client";
-import React from "react";
 
 interface RecommendedProps {
   data: User[];
 }
 
 export const Recommended = ({ data }: RecommendedProps) => {
-  return <div>Recommended</div>;
+  const { collapsed } = useSidebar((state) => state);
+
+  const showLabel = !collapsed && data.length > 0;
+
+  return (
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">Recommended</p>
+        </div>
+      )}
+    </div>
+  );
 };
