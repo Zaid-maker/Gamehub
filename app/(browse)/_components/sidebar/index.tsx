3adaf0e07dca57c687e77661c25d209137cdf3fb
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-16 14:07:48 +0000

    create Recommended component

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Wrapper } from "./wrapper";
+import { Recommended } from "./recommended";
 import { Toggle } from "./toggle";
+import { Wrapper } from "./wrapper";
 
 export const Sidebar = () => {
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended />
+      </div>
     </Wrapper>
   );
 };
---
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Recommended = () => {
+  return <div>Recommended</div>;
+};
