c8f62b2247e4dec6aac37b900949e61e9cca1cff
DevMirza <pzhafeez@gmail.com>
2023-12-17 13:59:45 +0000

    add Skeleton

--- app/(browse)/_components/sidebar/user-item.tsx
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -2,6 +2,7 @@
 
 import { LiveBadge } from "@/components/live-badge";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { UserAvatar } from "@/components/user-avatar";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
@@ -45,5 +46,16 @@ export const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
         </div>
       </Link>
     </Button>
+  );
+};
+
+export const UserItemSkeleton = () => {
+  return (
+    <li className="flex items-center gap-x-4 px-3 py-2">
+      <Skeleton className="min-h-[32px] min-w-[32px] rounded-full" />
+      <div className="flex-1">
+        <Skeleton className="h-6" />
+      </div>
+    </li>
   );
 };
