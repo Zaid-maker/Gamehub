2e00dcd734cb8360437c0da868c39e9a6ac87c04
DevMirza <pzhafeez@gmail.com>
2023-12-17 14:06:29 +0000

    wrap skeleton to layout to work

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Navbar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
+import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
 import { Container } from "./_components/container";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
@@ -8,7 +8,9 @@ const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
     <>
       <Navbar />
       <div className="flex h-full pt-20">
-        <Sidebar />
+        <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense>
         <Container>{children}</Container>
       </div>
     </>
