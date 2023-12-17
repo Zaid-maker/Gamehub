946202d80033fcd750bde531c370651a59fe0fcf
DevMirza <pzhafeez@gmail.com>
2023-12-17 18:23:29 +0000

    add followUser function to follow service

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -29,3 +29,43 @@ export const isFollowingUser = async (id: string) => {
     return false;
   }
 };
+
+export const followUser = async (id: string) => {
+  const self = await getSelf();
+
+  const otherUser = await db.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  if (otherUser.id === self.id) {
+    throw new Error("Cannot follow yourself");
+  }
+
+  const existingFollow = await db.follow.findFirst({
+    where: {
+      followerId: self.id,
+      followingId: otherUser.id,
+    },
+  });
+
+  if (existingFollow) {
+    throw new Error("Already Following");
+  }
+
+  const follow = await db.follow.create({
+    data: {
+      followerId: self.id,
+      followingId: otherUser.id,
+    },
+    include: {
+      following: true,
+      follower: true,
+    },
+  });
+
+  return follow;
+};
