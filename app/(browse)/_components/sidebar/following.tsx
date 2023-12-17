306fb2b7e4d87c70ccdd609a63f99d9f1d613a70
DevMirza <pzhafeez@gmail.com>
2023-12-17 20:47:19 +0000

    update component to show following

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import { useSidebar } from "@/store/use-sidebar";
 import { Follow, User } from "@prisma/client";
-import React from "react";
+import { UserItem } from "./user-item";
 
 interface FollowingProps {
   data: (Follow & { following: User })[];
 }
 
 export const Following = ({ data }: FollowingProps) => {
-  return <div>Following</div>;
+  const { collapsed } = useSidebar((state) => state);
+
+  if (!data.length) {
+    return null;
+  }
+
+  return (
+    <div>
+      {!collapsed && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">Following</p>
+        </div>
+      )}
+      <ul className="space-y-2 px-2">
+        {data.map((follow) => (
+          <UserItem
+            key={follow.following.id}
+            username={follow.following.username}
+            imageUrl={follow.following.imageUrl}
+            isLive={true}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
