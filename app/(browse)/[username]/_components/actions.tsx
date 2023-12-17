6818ffb6fcf10720d36d18892e4e1e0317368b83
DevMirza <pzhafeez@gmail.com>
2023-12-17 19:38:47 +0000

    following is working

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -3,6 +3,7 @@
 import { onFollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
+import { toast } from "sonner";
 
 interface ActionsProps {
   isFollowing: boolean;
@@ -12,13 +13,37 @@ interface ActionsProps {
 export const Actions = ({ isFollowing, userId }: ActionsProps) => {
   const [isPending, startTransition] = useTransition();
 
+  const handleFollow = () => {
+    startTransition(() => {
+      onFollow(userId)
+        .then((data) =>
+          toast.success(`You are now following ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  const handleUnfollow = () => {
+    startTransition(() => {
+      onFollow(userId)
+        .then((data) =>
+          toast.success(`You have unfollowed ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
   const onClick = () => {
-    onFollow("123");
+    if (isFollowing) {
+      handleUnfollow();
+    } else {
+      handleFollow();
+    }
   };
 
   return (
     <>
-      <Button disabled={isPending} variant="primary">
+      <Button disabled={isPending} onClick={onClick} variant="primary">
         {isFollowing ? "Unfollow" : "Follow"}
       </Button>
     </>
