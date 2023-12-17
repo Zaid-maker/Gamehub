570d2815688d3ad1144be746460a876ade7c945f
DevMirza <pzhafeez@gmail.com>
2023-12-17 20:23:32 +0000

    add a function to follow service

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -1,6 +1,25 @@
 import { getSelf } from "@/lib/auth-service";
 import { db } from "@/lib/db";
 
+export const getFollowedUsers = async () => {
+  try {
+    const self = await getSelf();
+
+    const followedUsers = db.follow.findMany({
+      where: {
+        followerId: self.id,
+      },
+      include: {
+        following: true,
+      },
+    });
+
+    return followedUsers;
+  } catch {
+    return [];
+  }
+};
+
 export const isFollowingUser = async (id: string) => {
   try {
     const self = await getSelf();
