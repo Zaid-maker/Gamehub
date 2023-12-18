2e5600db86644f0647d5071e6af6e44da573c0da
DevMirza <pzhafeez@gmail.com>
2023-12-18 10:42:33 +0000

    add more functions to block service

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -33,3 +33,99 @@ export const isBlockedByUser = async (id: string) => {
     return false;
   }
 };
+
+export const blockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) {
+    throw new Error("Cannot block yourself");
+  }
+
+  const otherUser = await db.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (existingBlock) {
+    throw new Error("Already blocked");
+  }
+
+  const block = await db.block.create({
+    data: {
+      blockerId: self.id,
+      blockedId: otherUser.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return block;
+};
+
+export const unblockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) {
+    throw new Error("Cannot unblock yourself");
+  }
+
+  const otherUser = await db.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (!existingBlock) {
+    throw new Error("Not blocked");
+  }
+
+  const unblock = await db.block.delete({
+    where: {
+      id: existingBlock.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return unblock;
+};
+
+export const getBlockedUsers = async () => {
+  const self = await getSelf();
+
+  const blockedUsers = await db.block.findMany({
+    where: {
+      blockerId: self.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return blockedUsers;
+};
