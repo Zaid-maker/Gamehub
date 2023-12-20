9b9adf36db06abbccc8751eeca51bdc0abf307d8
DevMirza <pzhafeez@gmail.com>
2023-12-20 18:37:28 +0000

    cleanup

--- app/(dashboard)/u/[username]/_components/sidebar/index.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/index.tsx
@@ -1,31 +1,10 @@
-import { getRecommended } from "@/lib/recommended-service";
-import { Recommended, RecommendedSkeleton } from "./recommended";
-import { Toggle, ToggleSkeleton } from "./toggle";
+import { Toggle } from "./toggle";
 import { Wrapper } from "./wrapper";
-import { getFollowedUsers } from "@/lib/follow-service";
-import { Following, FollowingSkeleton } from "./following";
 
 export const Sidebar = async () => {
-  const recommended = await getRecommended();
-  const following = await getFollowedUsers();
-
   return (
     <Wrapper>
       <Toggle />
-      <div className="space-y-4 pt-4 lg:pt-0">
-        <Following data={following} />
-        <Recommended data={recommended} />
-      </div>
     </Wrapper>
-  );
-};
-
-export const SidebarSkeleton = () => {
-  return (
-    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
-      <ToggleSkeleton />
-      <FollowingSkeleton />
-      <RecommendedSkeleton />
-    </aside>
   );
 };
