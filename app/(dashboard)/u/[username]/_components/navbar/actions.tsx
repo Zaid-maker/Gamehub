2add41c67bbc2acc3060f3c5d4426f7446d7088e
DevMirza <pzhafeez@gmail.com>
2023-12-20 18:33:33 +0000

    update actions component

--- app/(dashboard)/u/[username]/_components/navbar/actions.tsx
+++ app/(dashboard)/u/[username]/_components/navbar/actions.tsx
@@ -1,37 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { SignInButton, UserButton, currentUser } from "@clerk/nextjs";
-import { Clapperboard } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
 import Link from "next/link";
-import React from "react";
 
-export const Actions = async () => {
-  const user = await currentUser();
-
+export const Actions = () => {
   return (
-    <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
-      {!user && (
-        <SignInButton>
-          <Button size="sm" variant="primary">
-            Login
-          </Button>
-        </SignInButton>
-      )}
-      {!!user && (
-        <div className="flex items-center gap-x-4">
-          <Button
-            size="sm"
-            variant="ghost"
-            className="text-muted-foreground hover:text-primary"
-            asChild
-          >
-            <Link href={`/u/${user.username}`}>
-              <Clapperboard className="h-5 w-5 lg:mr-2" />
-              <span className="hidden lg:block">Dashboard</span>
-            </Link>
-          </Button>
-          <UserButton afterSignOutUrl="/" />
-        </div>
-      )}
+    <div className="flex items-center justify-end gap-x-2">
+      <Button
+        size="sm"
+        variant="ghost"
+        className="text-muted-foreground hover:text-primary"
+        asChild
+      >
+        <Link href="/">
+          <LogOut className="h-5 w-5 mr-2" />
+          Exit
+        </Link>
+      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
