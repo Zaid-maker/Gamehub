4196d9df5672380040e9593a30898ec4038dd8e2
DevMirza <pzhafeez@gmail.com>
2023-12-21 11:12:00 +0000

    update

--- app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
@@ -14,8 +14,8 @@ export const Wrapper = ({ children }: WrapperProps) => {
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
-        collapsed && "w-[70px]"
+        "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "lg:w-[70px]"
       )}
     >
       {children}
