342369081d7ea1d0a414cdfa7248d6544ff587e0
DevMirza <pzhafeez@gmail.com>
2023-12-21 11:33:01 +0000

    update toggle

--- app/(dashboard)/u/[username]/_components/sidebar/toggle.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/toggle.tsx
@@ -2,7 +2,6 @@
 
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
 import { useCreatorSidebar } from "@/store/use-creator-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 
@@ -39,14 +38,5 @@ export const Toggle = () => {
         </div>
       )}
     </>
-  );
-};
-
-export const ToggleSkeleton = () => {
-  return (
-    <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
-      <Skeleton className="h-6 w-[100px]" />
-      <Skeleton className="h-6 w-6" />
-    </div>
   );
 };
