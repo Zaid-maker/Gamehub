609c6009746c3286e381f88698fb97622455cece
DevMirza <pzhafeez@gmail.com>
2023-12-21 11:18:18 +0000

    update Navigation component

--- app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
@@ -1,7 +1,48 @@
 "use client";
 
-import React from "react";
+import { useUser } from "@clerk/nextjs";
+import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { NavItem } from "./nav-item";
 
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  const pathname = usePathname();
+  const { user } = useUser();
+
+  const routes = [
+    {
+      label: "Stream",
+      href: `/u/${user?.username}`,
+      icon: Fullscreen,
+    },
+    {
+      label: "Keys",
+      href: `/u/${user?.username}/keys`,
+      icon: KeyRound,
+    },
+    {
+      label: "Chat",
+      href: `/u/${user?.username}/chat`,
+      icon: MessageSquare,
+    },
+    {
+      label: "Community",
+      href: `/u/${user?.username}/community`,
+      icon: Users,
+    },
+  ];
+
+  return (
+    <div className="space-y-2 px-2 pt-4 lg:pt-0">
+      {routes.map((route) => (
+        <NavItem
+          key={route.href}
+          label={route.label}
+          icon={route.icon}
+          href={route.href}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </div>
+  );
 };
