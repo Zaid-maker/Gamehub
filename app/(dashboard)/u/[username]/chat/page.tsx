cbafbb8519096cfa87bad3c14820a2546f4ddcbc
DevMirza <pzhafeez@gmail.com>
2023-12-21 19:54:05 +0000

    create Chat route

---
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-semibold">Chat Settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
