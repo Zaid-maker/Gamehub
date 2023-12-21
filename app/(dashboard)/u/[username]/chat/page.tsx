b0ce8af0e1a10e6b698607b189708a31c4221e15
DevMirza <pzhafeez@gmail.com>
2023-12-21 20:02:11 +0000

    check of user streaming or not

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,6 +1,15 @@
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
 import React from "react";
 
-const Chat = () => {
+const Chat = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  //if (!stream) {
+  //  throw new Error("Stream not found");
+  //}
+
   return (
     <div className="p-6">
       <div className="mb-4">
