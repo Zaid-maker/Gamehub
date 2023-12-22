d3681c04c276a35e87eed7f73fd9c32804a89f5c
DevMirza <pzhafeez@gmail.com>
2023-12-22 09:56:28 +0000

    update and include Switch

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Switch } from "@/components/ui/switch";
+import { useTransition } from "react";
+
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
 
 interface ToggleCardProps {
@@ -13,10 +16,23 @@ export const ToggleCard = ({
   value = false,
   field,
 }: ToggleCardProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const onChange = () => {};
+
   return (
     <div className="rounded-xl bg-muted p-6">
       <div className="flex items-center justify-between">
         <p className="font-semibold shrink-0">{label}</p>
+        <div className="space-y-2">
+          <Switch
+            disabled={isPending}
+            onCheckedChange={onChange}
+            checked={value}
+          >
+            {value ? "On" : "Off"}
+          </Switch>
+        </div>
       </div>
     </div>
   );
