b0587a045bc8db52d4f0e00df20f5b6bcf476fb6
DevMirza <pzhafeez@gmail.com>
2023-12-22 15:30:52 +0000

    update the component

--- app/(dashboard)/u/[username]/keys/_components/key-card.tsx
+++ app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -1,11 +1,36 @@
 "use client";
 
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { CopyButton } from "./copy-button";
 
 interface KeyCardProps {
   value: string | null;
 }
 
 export const KeyCard = ({ value }: KeyCardProps) => {
-  return <div>KeyCard</div>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="rounded-xl bg-muted p-6">
+      <div className="flex items-start gap-x-10">
+        <p className="font-semibold shrink-0">Stream Key</p>
+        <div className="space-y-2 w-full">
+          <div className="w-full flex items-center gap-x-2">
+            <Input
+              value={value || ""}
+              type={show ? "text" : "password"}
+              disabled
+              placeholder="Stream key"
+            />
+            <CopyButton value={value || ""} />
+          </div>
+          <Button onClick={() => setShow(!show)} size="sm" variant="link">
+            {show ? "Hide" : "Show"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
