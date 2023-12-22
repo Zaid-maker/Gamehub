a20ebe4f2b4d3c04dfadc9760add4d0098848545
DevMirza <pzhafeez@gmail.com>
2023-12-22 22:49:44 +0000

    update

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/lib/db";
 import { WebhookReceiver } from "livekit-server-sdk";
 import { headers } from "next/headers";
 
-const reciever = new WebhookReceiver(
+const receiver = new WebhookReceiver(
   process.env.LIVEKIT_API_KEY!,
   process.env.LIVEKIT_API_SECRET!
 );
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
     return new Response("No authorization header", { status: 400 });
   }
 
-  const event = reciever.receive(body, authorization);
+  const event = receiver.receive(body, authorization);
 
   if (event.event === "ingress_started") {
     await db.stream.update({
