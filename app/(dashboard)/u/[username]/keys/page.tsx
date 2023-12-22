0511ff47897396f433616c5369ca6f13ad8b7822
DevMirza <pzhafeez@gmail.com>
2023-12-22 14:45:08 +0000

    create Keys route

---
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Keys = () => {
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-semibold">Stream Key Settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Keys;
