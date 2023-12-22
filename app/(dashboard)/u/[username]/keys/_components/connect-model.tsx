cf10df29628cb0aa944f325d12633d46ed013a22
DevMirza <pzhafeez@gmail.com>
2023-12-22 15:52:35 +0000

    update ConnectModel component

--- app/(dashboard)/u/[username]/keys/_components/connect-model.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-model.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+"use client";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { AlertTriangle } from "lucide-react";
+
+const RTMP = "RTMP";
+const WHIP = "WHIP";
 
 export const ConnectModel = () => {
-  return <div>ConnectModel</div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="primary">Generate Connection</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate Connection</DialogTitle>
+        </DialogHeader>
+        <Select>
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Ingess Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={RTMP}>RTMP</SelectItem>
+            <SelectItem value={WHIP}>WHIP</SelectItem>
+          </SelectContent>
+        </Select>
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning!</AlertTitle>
+          <AlertDescription>
+            This action will reset all active streams using the current
+            connection
+          </AlertDescription>
+        </Alert>
+      </DialogContent>
+    </Dialog>
+  );
 };
