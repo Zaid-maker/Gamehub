f0aa8c858428267620a7d837d3fe6d2f60cac84c
DevMirza <pzhafeez@gmail.com>
2023-12-22 14:59:41 +0000

    update

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,11 +1,14 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Keys = () => {
   return (
     <div className="p-6">
       <div className="mb-4">
-        <h1 className="text-2xl font-semibold">Stream Key Settings</h1>
+        <h1 className="text-2xl font-semibold">Stream Key & URLs</h1>
+        <Button variant="primary">Generate</Button>
       </div>
+      <div className="space-y-4"></div>
     </div>
   );
 };
