8f91dbcf3f57ab74ecfb7c50e7bc919404b01e39
DevMirza <pzhafeez@gmail.com>
2023-12-22 15:13:58 +0000

    create CopyButton component

---
+++ app/(dashboard)/u/[username]/keys/_components/copy-button.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+interface CopyClientProps {
+  value?: string;
+}
+
+export const CopyButton = ({ value }: CopyClientProps) => {
+  return <div>CopyButton</div>;
+};
--- app/(dashboard)/u/[username]/keys/_components/url-card.tsx
+++ app/(dashboard)/u/[username]/keys/_components/url-card.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/components/ui/input";
+import { CopyButton } from "./copy-button";
 
 interface UrlCardProps {
   value: string | null;
@@ -12,6 +13,7 @@ export const UrlCard = ({ value }: UrlCardProps) => {
         <div className="space-y-2 w-full">
           <div className="w-full flex items-center gap-x-2">
             <Input value={value || ""} disabled placeholder="Server URL" />
+            <CopyButton value={value || ""} />
           </div>
         </div>
       </div>
