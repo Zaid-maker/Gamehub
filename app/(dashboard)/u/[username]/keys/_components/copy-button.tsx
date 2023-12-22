8781699c08db43bd98ce89c4a53bf7f44908cabb
DevMirza <pzhafeez@gmail.com>
2023-12-22 15:20:05 +0000

    update CopyButton component

--- app/(dashboard)/u/[username]/keys/_components/copy-button.tsx
+++ app/(dashboard)/u/[username]/keys/_components/copy-button.tsx
@@ -1,11 +1,38 @@
 "use client";
 
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { CheckCheck, Copy } from "lucide-react";
+import React, { useState } from "react";
 
 interface CopyClientProps {
   value?: string;
 }
 
 export const CopyButton = ({ value }: CopyClientProps) => {
-  return <div>CopyButton</div>;
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopy = () => {
+    if (!value) return;
+
+    setIsCopied(true);
+
+    navigator.clipboard.writeText(value);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  const Icon = isCopied ? CheckCheck : Copy;
+
+  return (
+    <Button
+      onClick={onCopy}
+      disabled={!value || isCopied}
+      variant="ghost"
+      size="sm"
+    >
+      <Icon className="h-4 w-4" />
+    </Button>
+  );
 };
