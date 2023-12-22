2cf6823eea7ec8354d81159457349cb5293e17ef
DevMirza <pzhafeez@gmail.com>
2023-12-22 10:08:13 +0000

    create updateStream server action

---
+++ actions/stream.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import { Stream } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+export const updateStream = async (values: Partial<Stream>) => {
+  try {
+    const self = await getSelf();
+    const selfStream = await db.stream.findUnique({
+      where: {
+        userId: self.id,
+      },
+    });
+
+    if (!selfStream) {
+      throw new Error("Stream not found");
+    }
+
+    const validData = {
+      thumbnailUrl: values.thumbnailUrl,
+      name: values.name,
+      isChatEnabled: values.isChatEnabled,
+      isChatFollowersOnly: values.isChatFollowersOnly,
+      isChatDelayed: values.isChatDelayed,
+    };
+
+    const stream = await db.stream.update({
+      where: {
+        id: selfStream.id,
+      },
+      data: {
+        ...validData,
+      },
+    });
+
+    revalidatePath(`/u/${self.username}/chat`);
+    revalidatePath(`/u/${self.username}`);
+    revalidatePath(`/${self.username}`);
+
+    return stream;
+  } catch {
+    throw new Error("Internal Error");
+  }
+};
