8b6ba3a13f82a68cfd1b70b04f17309e81bf2c9a
DevMirza <pzhafeez@gmail.com>
2023-12-24 16:19:05 +0000

    create Video component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,6 +3,8 @@
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
+import { cn } from "@/lib/utils";
+import { Video } from "./video";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -26,8 +28,11 @@ export const StreamPlayer = ({
       <LiveKitRoom
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
         token={token}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
       >
-        Strean
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
       </LiveKitRoom>
     </>
   );
---
+++ components/stream-player/video.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+interface VideoProps {
+    hostName: string
+    hostIdentity: string
+}
+
+export const Video = ({ hostName, hostIdentity  }: VideoProps) => {
+  return <div>Video</div>;
+};
