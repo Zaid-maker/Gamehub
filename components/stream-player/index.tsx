e6781c30339ec2edc02f064acc9148bccdf0dc51
DevMirza <pzhafeez@gmail.com>
2023-12-24 19:47:54 +0000

    add Skeleton Loading for Video

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -4,7 +4,7 @@ import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
 import { cn } from "@/lib/utils";
-import { Video } from "./video";
+import { Video, VideoSkeleton } from "./video";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -20,7 +20,7 @@ export const StreamPlayer = ({
   const { token, name, identity } = useViewerToken(user.id);
 
   if (!token || !name || !identity) {
-    return <div>Cannot watch the stream</div>;
+    return <StreamPlayerSkeleton />;
   }
 
   return (
@@ -35,5 +35,17 @@ export const StreamPlayer = ({
         </div>
       </LiveKitRoom>
     </>
+  );
+};
+
+export const StreamPlayerSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+        {/*<HeaderSkeleton />*/}
+      </div>
+      <div className="col-span-1 bg-background">{/*<ChatSkeleton />*/}</div>
+    </div>
   );
 };
