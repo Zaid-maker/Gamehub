e4c89e3444a4f364d4f0bbf73be26540bd6cb3e5
DevMirza <pzhafeez@gmail.com>
2023-12-24 15:42:20 +0000

    create getUserById in user service

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -12,3 +12,16 @@ export const getUserByUsername = async (username: string) => {
 
   return user;
 };
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      stream: true,
+    },
+  });
+
+  return user;
+};
