d8a997097bff25b1eb6c5430194948c1a3ce6ebd
DevMirza <pzhafeez@gmail.com>
2023-12-24 16:09:18 +0000

    testing

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
+import { LiveKitRoom } from "@livekit/components-react";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -13,5 +15,20 @@ export const StreamPlayer = ({
   stream,
   isFollowing,
 }: StreamPlayerProps) => {
-  return <div>StreamPlayer</div>;
+  const { token, name, identity } = useViewerToken(user.id);
+
+  if (!token || !name || !identity) {
+    return <div>Cannot watch the stream</div>;
+  }
+
+  return (
+    <>
+      <LiveKitRoom
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        token={token}
+      >
+        Strean
+      </LiveKitRoom>
+    </>
+  );
 };
