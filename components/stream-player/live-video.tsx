397d90e145e307b3a41e4b51917d8c0f1b330a27
DevMirza <pzhafeez@gmail.com>
2023-12-25 08:20:59 +0000

    show live video

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,10 +1,17 @@
 import { Participant } from "livekit-client";
-import React from "react";
+import React, { useRef } from "react";
 
 interface LiveVideoProps {
   participant: Participant;
 }
 
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
-  return <div>LiveVideo</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={wrapperRef} className="relative h-full flex">
+      <video ref={videoRef} width="100%" />
+    </div>
+  );
 };
