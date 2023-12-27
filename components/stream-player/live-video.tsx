bc517c69cc07179ca5f378f3c40c90691cd9d432
DevMirza <pzhafeez@gmail.com>
2023-12-27 17:03:42 +0000

    create fullscreen-control component

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,6 +1,7 @@
 import { useTracks } from "@livekit/components-react";
 import { Participant, Track } from "livekit-client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { FullscreenControl } from "./fullscreen-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -9,6 +10,16 @@ interface LiveVideoProps {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = () => {
+    if (isFullscreen) {
+      document.exitFullscreen();
+    } else if (wrapperRef?.current) {
+      wrapperRef.current.requestFullscreen();
+    }
+  };
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -21,6 +32,14 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videoRef} width="100%" />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <FullscreenControl
+            isFullscreen={isFullscreen}
+            onToggle={toggleFullscreen}
+          />
+        </div>
+      </div>
     </div>
   );
 };
