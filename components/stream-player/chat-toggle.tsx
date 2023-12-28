fdd716f32196f9b7f707f0805bdb31017188c110
DevMirza <pzhafeez@gmail.com>
2023-12-28 16:47:26 +0000

    update ChatToggle

--- components/stream-player/chat-toggle.tsx
+++ components/stream-player/chat-toggle.tsx
@@ -1,7 +1,35 @@
 "use client";
 
+import { useChatSidebar } from "@/store/use-chat-sidebat";
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import React from "react";
+import { Hint } from "../hint";
+import { Button } from "../ui/button";
 
 export const ChatToggle = () => {
-  return <div>ChatToggle</div>;
+  const { collapsed, onExpand, onCollapse } = useChatSidebar((state) => state);
+
+  const Icon = collapsed ? ArrowLeftFromLine : ArrowRightFromLine;
+
+  const onToggle = () => {
+    if (collapsed) {
+      onExpand();
+    } else {
+      onCollapse();
+    }
+  };
+
+  const label = collapsed ? "Expand" : "Collapse";
+
+  return (
+    <Hint label={label} side="left" asChild>
+      <Button
+        onClick={onToggle}
+        variant="ghost"
+        className="h-auto p-2 hover:bg-white/10 hover:text-primary bg-transparent"
+      >
+        <Icon className="h-4 w-4" />
+      </Button>
+    </Hint>
+  );
 };
