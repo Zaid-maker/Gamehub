dd24d370deca54ec5d27ddeef993b9c2b2a32308
DevMirza <pzhafeez@gmail.com>
2023-12-28 19:49:59 +0000

    update ChatMessage and add date-fns

--- components/stream-player/chatMesage.tsx
+++ components/stream-player/chatMesage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReceivedChatMessage } from "@livekit/components-react";
-import React from "react";
+import { format } from "date-fns";
 
 interface ChatMessageProps {
   data: ReceivedChatMessage;
@@ -9,13 +9,13 @@ interface ChatMessageProps {
 
 export const ChatMessage = ({ data }: ChatMessageProps) => {
   return (
-    <div>
-      <p></p>
-      <div>
-        <p>
-          <span className="truncate"></span>
+    <div className="flex gap-2 p-2 rounded-md hover:bg-white/5">
+      <p className="text-sm text-white/40">{format(data.timestamp, "HH:MM")}</p>
+      <div className="flex flex-wrap items-baseline gap-1 grow">
+        <p className="text-sm font-semibold whitespace-nowrap">
+          <span className="truncate">{data.from?.name}</span>:
         </p>
-        <p>{data.message}</p>
+        <p className="text-sm break-all">{data.message}</p>
       </div>
     </div>
   );
