76b2796aac6ef26912fadcfd7097e2764a46acfe
DevMirza <pzhafeez@gmail.com>
2023-12-28 18:57:37 +0000

    create ChatInfo component

---
+++ components/stream-player/chat-info.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface ChatInfoProps {
+  isDelayed: boolean;
+  isFollowersOnly: boolean;
+}
+
+export const ChatInfo = ({ isDelayed, isFollowersOnly }: ChatInfoProps) => {
+  return <div>ChatInfo</div>;
+};
