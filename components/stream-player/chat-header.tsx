2f9fc7aad7ca5f24e46629adbe9a27d1a4edf0e9
DevMirza <pzhafeez@gmail.com>
2023-12-28 16:35:44 +0000

    create ChatToggle and VariantToggle components

--- components/stream-player/chat-header.tsx
+++ components/stream-player/chat-header.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import React from "react";
+import { ChatToggle } from "./chat-toggle";
+import { VariantToggle } from "./variant-toggle";
 
 export const ChatHeader = () => {
   return (
     <div className="relative p-3 border-b">
-      <div className="absolute left-2 top-2 hidden lg:block">ChatToggle</div>
+      <div className="absolute left-2 top-2 hidden lg:block">
+        <ChatToggle />
+      </div>
       <p className="font-semibold text-priamry text-center">Stream Chat</p>
-      <div className="absolute right-2 top-2">VariantToggle</div>
+      <div className="absolute right-2 top-2">
+        <VariantToggle />
+      </div>
     </div>
   );
 };
---
+++ components/stream-player/variant-toggle.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const VariantToggle = () => {
+  return <div>VariantToggle</div>;
+};
