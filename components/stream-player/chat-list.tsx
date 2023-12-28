1e8806949fcac1f48b87449a3514c4e916a1748b
DevMirza <pzhafeez@gmail.com>
2023-12-28 19:38:50 +0000

    update ChatList to show chat

--- components/stream-player/chat-list.tsx
+++ components/stream-player/chat-list.tsx
@@ -7,5 +7,21 @@ interface ChatListProps {
 }
 
 export const ChatList = ({ messages, isHidden }: ChatListProps) => {
-  return <div>ChatList</div>;
+  if (isHidden || !messages || messages.length === 0) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-sm text-muted-foreground">
+          {isHidden ? "Chat is disabled" : "Welcome to Chat!"}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-1 flex-col-reverse overflow-y-auto p-3 h-full">
+      {messages.map((message) => (
+        <div key={message.timestamp}>CHAT</div>
+      ))}
+    </div>
+  );
 };
