153a2fa25818b148e817b56e63d2697dd918d3be
DevMirza <pzhafeez@gmail.com>
2023-12-28 19:57:17 +0000

    update to show colors on username

--- components/stream-player/chatMesage.tsx
+++ components/stream-player/chatMesage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { stringToColor } from "@/lib/utils";
 import { ReceivedChatMessage } from "@livekit/components-react";
 import { format } from "date-fns";
 
@@ -8,12 +9,17 @@ interface ChatMessageProps {
 }
 
 export const ChatMessage = ({ data }: ChatMessageProps) => {
+  const color = stringToColor(data.from?.name || "");
+
   return (
     <div className="flex gap-2 p-2 rounded-md hover:bg-white/5">
       <p className="text-sm text-white/40">{format(data.timestamp, "HH:MM")}</p>
       <div className="flex flex-wrap items-baseline gap-1 grow">
         <p className="text-sm font-semibold whitespace-nowrap">
-          <span className="truncate">{data.from?.name}</span>:
+          <span className="truncate" style={{ color: color }}>
+            {data.from?.name}
+          </span>
+          :
         </p>
         <p className="text-sm break-all">{data.message}</p>
       </div>
