c0cd9ce4c6ed6b81051ba385362cd3a79ed804fc
DevMirza <pzhafeez@gmail.com>
2023-12-30 19:22:37 +0000

    create Community route

---
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Community = () => {
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Community Settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Community;
