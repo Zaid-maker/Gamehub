a51afc06d15ee0ad3e83ab4f8e1fa9c16e2a2369
DevMirza <pzhafeez@gmail.com>
2023-12-30 20:06:38 +0000

    update CommunityItem

--- components/stream-player/community-item.tsx
+++ components/stream-player/community-item.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import React from "react";
+import { onBlock } from "@/actions/block";
+import { cn, stringToColor } from "@/lib/utils";
+import { MinusCircle } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
+import { Hint } from "../hint";
+import { Button } from "../ui/button";
 
 interface CommunityItemProps {
   hostName: string;
@@ -15,5 +21,42 @@ export const CommunityItem = ({
   participantName,
   participantIdentity,
 }: CommunityItemProps) => {
-  return <div>CommunityItem</div>;
+  const [isPending, startTransition] = useTransition();
+
+  const color = stringToColor(participantName || "");
+  const isSelf = participantName === viewerName;
+  const isHost = viewerName === hostName;
+
+  const handleBlock = () => {
+    if (!participantName || isSelf || !isHost) return;
+
+    startTransition(() => {
+      onBlock(participantIdentity)
+        .then(() => toast.success(`Blocked ${participantName}`))
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  return (
+    <div
+      className={cn(
+        "group flex items-center justify-between w-full p-2 rounded-md text-sm hover:bg-white/5",
+        isPending && "opacity-50 pointer-events-none"
+      )}
+    >
+      <p style={{ color: color }}>{participantName}</p>
+      {isHost && !isSelf && (
+        <Hint label="Block">
+          <Button
+            variant="ghost"
+            disabled={isPending}
+            onClick={handleBlock}
+            className="h-auto w-auto p-1 opacity-0 group-hover:opacity-100 transition"
+          >
+            <MinusCircle className="h-4 w-4 text-muted-foreground" />
+          </Button>
+        </Hint>
+      )}
+    </div>
+  );
 };
