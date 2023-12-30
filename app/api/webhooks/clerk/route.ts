969553d383373a4b2afe89598d63a6961990f92c
DevMirza <pzhafeez@gmail.com>
2023-12-30 20:24:15 +0000

    reset Strean key and URL on user get deleted

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,7 +1,9 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
+
 import { db } from "@/lib/db";
+import { resetIngresses } from "@/actions/ingress";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -79,7 +81,7 @@ export async function POST(req: Request) {
   }
 
   if (eventType === "user.deleted") {
-    // await resetIngresses(payload.data.id);
+    await resetIngresses(payload.data.id);
 
     await db.user.delete({
       where: {
