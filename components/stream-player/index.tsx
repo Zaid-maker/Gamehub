ecb42ecf155c1053ff7ab1f76c63866433ce7556
DevMirza <pzhafeez@gmail.com>
2024-01-01 19:43:38 +0000

    update Header to show details

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -8,7 +8,7 @@ import { Stream, User } from "@prisma/client";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Video, VideoSkeleton } from "./video";
-import { Header } from "./header";
+import { Header, HeaderSkeleton } from "./header";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -75,7 +75,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/*<HeaderSkeleton />*/}
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
