501f55ee92a73bde6647f8d67bd06a347db14058
DevMirza <pzhafeez@gmail.com>
2024-01-02 19:23:26 +0000

    update Actions component

--- components/stream-player/actions.tsx
+++ components/stream-player/actions.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React from "react";
+import { Button } from "../ui/button";
+import { Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface ActionsProps {
   hostIdentity: string;
@@ -13,5 +16,15 @@ export const Actions = ({
   isFollowing,
   isHost,
 }: ActionsProps) => {
-  return <div>Actions</div>;
+  return (
+    <Button variant="primary" size="sm" className="w-full lg:w-auto">
+      <Heart
+        className={cn(
+          "h-4, w-4 mr-2",
+          isFollowing ? "fill-white" : "fill-none"
+        )}
+      />
+      {isFollowing ? "Unfollow" : "Follow"}
+    </Button>
+  );
 };
