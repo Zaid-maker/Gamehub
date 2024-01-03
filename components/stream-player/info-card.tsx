d7c9e82fd9a7d090ec5dc922af77a2e26504fb5b
DevMirza <pzhafeez@gmail.com>
2024-01-03 18:00:38 +0000

    update InfoCard

--- components/stream-player/info-card.tsx
+++ components/stream-player/info-card.tsx
@@ -2,6 +2,8 @@
 
 import { Pencil } from "lucide-react";
 import React from "react";
+import { Separator } from "../ui/separator";
+import Image from "next/image";
 
 interface InfoCardProps {
   name: string;
@@ -10,7 +12,12 @@ interface InfoCardProps {
   viewerIdentity: string;
 }
 
-export const InfoCard = ({}: InfoCardProps) => {
+export const InfoCard = ({
+  name,
+  thumbnailUrl,
+  hostIdentity,
+  viewerIdentity,
+}: InfoCardProps) => {
   return (
     <div className="px-4">
       <div className="rounded=xl bg-background">
@@ -25,6 +32,26 @@ export const InfoCard = ({}: InfoCardProps) => {
             <p className="text-muted-foreground text-xs lg:text-sm">
               Maximize your visibility
             </p>
+          </div>
+        </div>
+        <Separator />
+        <div className="p-4 lg:p-6 space-y-4">
+          <div>
+            <h3 className="text-sm text-muted-foreground mb-2">Name</h3>
+            <p className="text-sm font-semibold">{name}</p>
+          </div>
+          <div>
+            <h3 className="text-sm text-muted-foreground mb-2">Thumbnail</h3>
+            {thumbnailUrl && (
+              <div className="relative aspect-video rounded-md overflow-hidden w-[200px] border border-white/10">
+                <Image
+                  fill
+                  src={thumbnailUrl}
+                  alt={name}
+                  className="object-cover"
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
