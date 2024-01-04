80e9a036f27bbe70ab5cd4dbb22e46aae3594760
DevMirza <pzhafeez@gmail.com>
2024-01-04 15:02:27 +0000

    update InfoModal to show modal

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -1,7 +1,45 @@
 "use client";
 
 import React from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
 
 export const InfoModal = () => {
-  return <div>InfoModal</div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link" size="sm" className="ml-auto">
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Stream Info</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-14">
+          <div className="space-y-2">
+            <Input disabled placeholder="Stream Name" onChange={() => {}} />
+          </div>
+          <div className="flex justify-between">
+            <DialogClose asChild>
+              <Button type="button" variant="ghost">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled variant="primary" type="submit">
+              Save
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 };
