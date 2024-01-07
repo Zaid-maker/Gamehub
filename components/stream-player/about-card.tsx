b4d5f40b19a62202894f02abe2fb89c99bff5747
DevMirza <pzhafeez@gmail.com>
2024-01-07 14:13:47 +0000

    update

--- components/stream-player/about-card.tsx
+++ components/stream-player/about-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { VerifiedMark } from "../verified-mark";
 
 interface AboutCardProps {
   hostName: string;
@@ -10,6 +11,36 @@ interface AboutCardProps {
   followedByCount: number;
 }
 
-export const AboutCard = ({}: AboutCardProps) => {
-  return <div>AboutCard</div>;
+export const AboutCard = ({
+  hostName,
+  hostIdentity,
+  viewerIdentity,
+  bio,
+  followedByCount,
+}: AboutCardProps) => {
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = viewerIdentity === hostAsViewer;
+
+  const followedByLabel = followedByCount === 1 ? "Follower" : "Followers";
+
+  return (
+    <div className="px-4">
+      <div className="group rounded-xl bg-background p-6 lg:p-10 flex flex-col gap-y-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-x-2 font-semibold text-lg lg:text-2xl">
+            About {hostName}
+            <VerifiedMark />
+          </div>
+          {isHost && <p>Edit</p>}
+        </div>
+        <div className="text-sm text-muted-foreground">
+          <span className="text-semibold text-primary">{followedByCount}</span>{" "}
+          {followedByLabel}
+        </div>
+        <p className="text-sm">
+          {bio || "This user prefers to keep an air of mystery about them."}
+        </p>
+      </div>
+    </div>
+  );
 };
