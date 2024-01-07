e8200a820552023d2a9c408219e72c6a7ff3e0b3
DevMirza <pzhafeez@gmail.com>
2024-01-07 15:19:02 +0000

    BioModal component for about

---
+++ components/stream-player/bio-modal copy.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { updateStream } from "@/actions/stream";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { UploadDropzone } from "@/lib/uploadthing";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { ElementRef, useRef, useState, useTransition } from "react";
+import { toast } from "sonner";
+import { Hint } from "../hint";
+import { Trash } from "lucide-react";
+
+interface BioModalProps {
+  initialName: string;
+  initialThumbnailUrl: string | null;
+}
+
+export const BioModal = ({
+  initialName,
+  initialThumbnailUrl,
+}: BioModalProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const [isPending, startTransition] = useTransition();
+  const [name, setName] = useState(initialName);
+  const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
+
+  const onRemove = () => {
+    startTransition(() => {
+      updateStream({ thumbnailUrl: null })
+        .then(() => {
+          toast.success("Thumbnail removed");
+          setThumbnailUrl("");
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong!"));
+    });
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    startTransition(() => {
+      updateStream({ name: name })
+        .then(() => {
+          toast.success("Stream settings updated");
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong!"));
+    });
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link" size="sm" className="ml-auto">
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Stream Info</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={onSubmit} className="space-y-14">
+          <div className="space-y-2">
+            <Label>Name</Label>
+            <Input
+              disabled={isPending}
+              placeholder="Stream Name"
+              onChange={onChange}
+              value={name}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label>Thumbnail</Label>
+            {thumbnailUrl ? (
+              <div className="relative aspect-video rounded-xl overflow-hidden border border-white/10">
+                <div className="absolute top-2 right-2 z-[10]">
+                  <Hint label="Remove thumbnail" asChild side="left">
+                    <Button
+                      type="button"
+                      disabled={isPending}
+                      onClick={onRemove}
+                      className="h-auto w-auto p-1.5"
+                    >
+                      <Trash className="h-4 w-4" />
+                    </Button>
+                  </Hint>
+                </div>
+                <Image
+                  alt="thumbnail"
+                  src={thumbnailUrl}
+                  className="object-cover"
+                  fill
+                />
+              </div>
+            ) : (
+              <div className="rounded-xl border outline-dashed outline-muted">
+                <UploadDropzone
+                  endpoint="thumbnailUploader"
+                  appearance={{
+                    label: {
+                      color: "#FFFFFF",
+                    },
+                    allowedContent: {
+                      color: "#FFFFFF",
+                    },
+                  }}
+                  onClientUploadComplete={(res) => {
+                    setThumbnailUrl(res?.[0]?.url);
+                    router.refresh();
+                    closeRef?.current?.click();
+                  }}
+                />
+              </div>
+            )}
+          </div>
+          <div className="flex justify-between">
+            <DialogClose ref={closeRef} asChild>
+              <Button type="button" variant="ghost">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={isPending} variant="primary" type="submit">
+              Save
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
