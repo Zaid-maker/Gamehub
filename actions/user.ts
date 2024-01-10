4fe668101f34e968277b597b0e5f57efb19ca9d7
DevMirza <pzhafeez@gmail.com>
2024-01-10 18:37:01 +0000

    update

--- actions/user.ts
+++ actions/user.ts
@@ -6,27 +6,19 @@ import { User } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export const updateUser = async (values: Partial<User>) => {
-  try {
-    const self = await getSelf();
+  const self = await getSelf();
 
-    const validateData = {
-      bio: values.bio,
-    };
+  const validData = {
+    bio: values.bio,
+  };
 
-    const user = await db.user.update({
-      where: {
-        id: self.id,
-      },
-      data: {
-        ...validateData,
-      },
-    });
+  const user = await db.user.update({
+    where: { id: self.id },
+    data: { ...validData },
+  });
 
-    revalidatePath(`/${self.username}`);
-    revalidatePath(`/u/${self.username}`);
+  revalidatePath(`/${self.username}`);
+  revalidatePath(`/u/${self.username}`);
 
-    return user;
-  } catch (error) {
-    throw new Error("Something went wrong");
-  }
+  return user;
 };
