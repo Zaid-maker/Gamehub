90e99d3eaba8511b3f6dc922aae61fa5cb1601f6
DevMirza <pzhafeez@gmail.com>
2024-01-11 20:23:56 +0000

    update

--- app/(browse)/(home)/_components/results.tsx
+++ app/(browse)/(home)/_components/results.tsx
@@ -1,8 +1,20 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { getStreams } from "@/lib/feed-service";
 import React from "react";
 
-export const Results = () => {
-  return <div>Results</div>;
+export const Results = async () => {
+  const data = await getStreams();
+
+  return (
+    <div>
+      <h2 className="text-lg font-semibold mb-4">
+        Streams we think you&apos;ll like
+      </h2>
+      {data.length === 0 && (
+        <div className="text-muted-foreground text-sm">No streams found.</div>
+      )}
+    </div>
+  );
 };
 
 export const ResultsSkeleton = () => {
