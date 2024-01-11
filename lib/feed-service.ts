4461676e700b4450bb1afd1b140a55ad1775c080
DevMirza <pzhafeez@gmail.com>
2024-01-11 20:12:24 +0000

    add Fead service

---
+++ lib/feed-service.ts
@@ -0,0 +1,67 @@
+import { getSelf } from "./auth-service";
+import { db } from "./db";
+
+export const getStreams = async () => {
+  let userId;
+
+  try {
+    const self = await getSelf();
+
+    userId = self.id;
+  } catch (error) {
+    userId = null;
+  }
+
+  let streams = [];
+
+  if (userId) {
+    streams = await db.stream.findMany({
+      where: {
+        user: {
+          NOT: {
+            blocking: {
+              some: {
+                blockedId: userId,
+              },
+            },
+          },
+        },
+      },
+      select: {
+        id: true,
+        user: true,
+        isLive: true,
+        name: true,
+        thumbnailUrl: true,
+      },
+      orderBy: [
+        {
+          isLive: "desc",
+        },
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
+  } else {
+    streams = await db.stream.findMany({
+      select: {
+        id: true,
+        user: true,
+        isLive: true,
+        name: true,
+        thumbnailUrl: true,
+      },
+      orderBy: [
+        {
+          isLive: "desc",
+        },
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
+  }
+
+  return streams;
+};
