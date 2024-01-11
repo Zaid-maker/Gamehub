886e0f416b27cffd6960c12be491268bb4e95cb4
DevMirza <pzhafeez@gmail.com>
2024-01-11 20:39:32 +0000

    update ResultsCard

--- app/(browse)/(home)/_components/result-card.tsx
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -1,4 +1,6 @@
+import { UserAvatar } from "@/components/user-avatar";
 import { Stream, User } from "@prisma/client";
+import Link from "next/link";
 import React from "react";
 
 interface ResultsCardProps {
@@ -11,5 +13,24 @@ interface ResultsCardProps {
 }
 
 export const ResultsCard = ({ data }: ResultsCardProps) => {
-  return <div>ResultsCard</div>;
+  return (
+    <Link href={`/${data.user.username}`}>
+      <div className="h-dull w-full space-y-4">
+        Thumbnail
+        <div className="flex gap-x-3">
+          <UserAvatar
+            username={data.user.username}
+            imageUrl={data.user.imageUrl}
+            isLive={data.isLive}
+          />
+          <div className="flex flex-col text-sm overflow-hidden">
+            <p className="truncate font-semibold hover:text-blue-500">
+              {data.name}
+            </p>
+            <p className="text-muted-foreground">{data.user.username}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 };
