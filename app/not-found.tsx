0838e99840df5aa548491b9b5a4e23138becb571
DevMirza <pzhafeez@gmail.com>
2024-01-11 19:16:47 +0000

    add error and not-found for home aswell

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground">
+      <h1 className="text-4xl">404</h1>
+      <p>We couldn&apos;t find the page you were looking for.</p>
+      <Button variant="secondary" asChild>
+        <Link href="/">Go Back Home</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default NotFoundPage;
