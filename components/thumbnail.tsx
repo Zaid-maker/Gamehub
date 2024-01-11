8c1d5a32af6831c665f3a459aca6c540034531ce
DevMirza <pzhafeez@gmail.com>
2024-01-11 20:48:32 +0000

    update Thumbnail

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import { Skeleton } from "./ui/skeleton";
 
 export const Thumbnail = () => {
-  return <div>Thumbnail</div>;
+  let content;
+
+  return (
+    <div className="group aspect-video relative rounded-md cursor-pointer">
+      <div className="rounded-md absolute inset-0 bg-blue-600 opacity-0 group-hover:opacity-100 flex items-center justify-center">
+        {content}
+      </div>
+    </div>
+  );
 };
 
 export const ThumbnailSkeleton = () => {
