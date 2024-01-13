4100de0886ee56a6f6383b88a200d70c3aac4c55
DevMirza <pzhafeez@gmail.com>
2024-01-13 19:54:15 +0000

    update Thumbnail

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
+import { LiveBadge } from "./live-badge";
+import { UserAvatar } from "./user-avatar";
+import Image from "next/image";
 
 interface ThumbnailProps {
   src: string | null;
@@ -16,10 +19,38 @@ export const Thumbnail = ({
 }: ThumbnailProps) => {
   let content;
 
+  if (!src) {
+    content = (
+      <div className="bg-background flex flex-col items-center justify-center gap-y-4 h-full w-full transition-transform group-hover:translate-x-2 group-hover:translate-y-2 rounded-md">
+        <UserAvatar
+          size="lg"
+          showBadge
+          username={username}
+          imageUrl={fallback}
+          isLive={isLive}
+        />
+      </div>
+    );
+  } else {
+    content = (
+      <Image
+        src={src}
+        fill
+        alt="thumbnail"
+        className="object-cover transition-transform group-hover:translate-x-2 group-hover:-translate-y-2 rounded-md"
+      />
+    );
+  }
+
   return (
     <div className="group aspect-video relative rounded-md cursor-pointer">
       <div className="rounded-md absolute inset-0 bg-blue-600 opacity-0 group-hover:opacity-100 flex items-center justify-center">
         {content}
+        {isLive && src && (
+          <div className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform">
+            <LiveBadge />
+          </div>
+        )}
       </div>
     </div>
   );
