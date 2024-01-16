10452f8c60967a08839c6f7165694d6f26dfad11
DevMirza <pzhafeez@gmail.com>
2024-01-16 18:13:50 +0000

    update user service

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -5,8 +5,22 @@ export const getUserByUsername = async (username: string) => {
     where: {
       username,
     },
-    include: {
-      stream: true,
+    select: {
+      id: true,
+      username: true,
+      bio: true,
+      imageUrl: true,
+      stream: {
+        select: {
+          id: true,
+          isLive: true,
+          isChatDelayed: true,
+          isChatEnabled: true,
+          isChatFollowersOnly: true,
+          thumbnailUrl: true,
+          name: true,
+        },
+      },
       _count: {
         select: {
           followedBy: true,
