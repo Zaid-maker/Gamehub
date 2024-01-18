c69cf7c3c47b11b55ba26ac0bc54362791f2cf96
DevMirza <pzhafeez@gmail.com>
2024-01-18 14:59:48 +0000

    update

--- app/(browse)/search/_components/results-card.tsx
+++ app/(browse)/search/_components/results-card.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { VerifiedMark } from "@/components/verified-mark";
 import { User } from "@prisma/client";
 import { formatDistanceToNow } from "date-fns";
+import Link from "next/link";
 import React from "react";
 
 interface ResultsCardProps {
@@ -18,30 +19,32 @@ interface ResultsCardProps {
 
 export const ResultsCard = ({ data }: ResultsCardProps) => {
   return (
-    <div className="w-full flex gap-x-4">
-      <div className="relative h-[9rem] w-[16rem]">
-        <Thumbnail
-          src={data.thumbnailUrl}
-          fallback={data.user.imageUrl}
-          isLive={data.isLive}
-          username={data.user.username}
-        />
-      </div>
-      <div className="space-y-1">
-        <div className="flex items-center gap-x-2">
-          <p className="font-bold text-lg cursor-pointer hover:text-blue-500">
-            {data.user.username}
-          </p>
-          <VerifiedMark />
+    <Link href={`/${data.user.username}`}>
+      <div className="w-full flex gap-x-4">
+        <div className="relative h-[9rem] w-[16rem]">
+          <Thumbnail
+            src={data.thumbnailUrl}
+            fallback={data.user.imageUrl}
+            isLive={data.isLive}
+            username={data.user.username}
+          />
         </div>
-        <p className="text-sm text-muted-foreground">{data.name}</p>
-        <p className="text-sm text-muted-foreground">
-          {formatDistanceToNow(new Date(data.updatedAt), {
-            addSuffix: true,
-          })}
-        </p>
+        <div className="space-y-1">
+          <div className="flex items-center gap-x-2">
+            <p className="font-bold text-lg cursor-pointer hover:text-blue-500">
+              {data.user.username}
+            </p>
+            <VerifiedMark />
+          </div>
+          <p className="text-sm text-muted-foreground">{data.name}</p>
+          <p className="text-sm text-muted-foreground">
+            {formatDistanceToNow(new Date(data.updatedAt), {
+              addSuffix: true,
+            })}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
