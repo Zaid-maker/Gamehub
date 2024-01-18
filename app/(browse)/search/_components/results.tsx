1c663aceeb27fd0c449a80c2ab2678408e8708f1
DevMirza <pzhafeez@gmail.com>
2024-01-18 11:32:07 +0000

    create search service to fetch

--- app/(browse)/search/_components/results.tsx
+++ app/(browse)/search/_components/results.tsx
@@ -1,15 +1,30 @@
+import { getSearch } from "@/lib/search-service";
 import React from "react";
 
 interface ResultsProps {
   term?: string;
 }
 
-export const Results = ({ term }: ResultsProps) => {
+export const Results = async ({ term }: ResultsProps) => {
+  const data = await getSearch(term);
+
   return (
     <div>
       <h2 className="text-lg font-semibold mb-4">
         Results for term &quot;{term}&quot;
       </h2>
+      {data.length === 0 && (
+        <p className="text-muted-foreground text-sm">
+          No results found. Try searching for something else
+        </p>
+      )}
+      <div className="flex flex-col gap-y-4">
+        {data.map((result) => (
+          <div key={result.id}>
+            <p>{result.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
