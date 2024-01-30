e4a030ee9cedee68bce63407d1d08d91e54c4687
DevMirza <pzhafeez@gmail.com>
2024-01-30 09:36:24 +0000

    demo table

--- app/(dashboard)/u/[username]/community/page.tsx
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { DataTable } from "./_components/data-table";
+import { Payment, columns } from "./_components/column";
 
-const Community = () => {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const Community = async () => {
+  const data = await getData();
+
   return (
     <div className="p-6">
       <div className="mb-4">
         <h1 className="text-2xl font-bold">Community Settings</h1>
+        <DataTable columns={columns} data={data} />
       </div>
     </div>
   );
