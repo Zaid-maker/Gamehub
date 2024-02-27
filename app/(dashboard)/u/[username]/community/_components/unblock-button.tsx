c11b7269f483befb4ba2ca39fa2e6f762cee614c
DevMirza <pzhafeez@gmail.com>
2024-02-27 10:05:07 +0000

    update unblockbutton

--- app/(dashboard)/u/[username]/community/_components/unblock-button.tsx
+++ app/(dashboard)/u/[username]/community/_components/unblock-button.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { onUnblock } from "@/actions/block";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
-export const UnblockButton = () => {
-  return <div>UnblockButton</div>;
+interface UnblockButtonProps {
+  userId: string;
+}
+
+export const UnblockButton = ({ userId }: UnblockButtonProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const onClick = () => {
+    startTransition(() => {
+      onUnblock(userId)
+        .then((result) =>
+          toast.success(`User ${result.blocked.username} unblocked`)
+        )
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+  return (
+    <Button
+      disabled={isPending}
+      onClick={onClick}
+      variant="link"
+      size="sm"
+      className="text-blue-500 w-full"
+    >
+      Unblcok
+    </Button>
+  );
 };
