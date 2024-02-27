9d5ec7b6d6ee069c485f5e5e3f099cd7f2640605
DevMirza <pzhafeez@gmail.com>
2024-02-27 09:58:42 +0000

    update coloumn

--- app/(dashboard)/u/[username]/community/_components/column.tsx
+++ app/(dashboard)/u/[username]/community/_components/column.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "@/components/user-avatar";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { UnblockButton } from "./unblock-button";
 
 export type BlockedUser = {
   id: string;
@@ -13,10 +17,39 @@ export type BlockedUser = {
 export const columns: ColumnDef<BlockedUser>[] = [
   {
     accessorKey: "username",
-    header: "username",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Username
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-4">
+        <UserAvatar
+          username={row.original.username}
+          imageUrl={row.original.imageUrl}
+        />
+        <span>{row.original.username}</span>
+      </div>
+    ),
   },
   {
     accessorKey: "createdAt",
-    header: "Created At",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Date blocked
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <UnblockButton userId={row.original.userId} />,
   },
 ];
---
+++ app/(dashboard)/u/[username]/community/_components/unblock-button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UnblockButton = () => {
+  return <div>UnblockButton</div>;
+};
