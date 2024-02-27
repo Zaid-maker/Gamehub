292117f0690f7c30a4246ae4bb9501a1be611d46
DevMirza <pzhafeez@gmail.com>
2024-02-27 10:52:29 +0000

    Previous and Next Button

--- app/(dashboard)/u/[username]/community/_components/data-table.tsx
+++ app/(dashboard)/u/[username]/community/_components/data-table.tsx
@@ -117,6 +117,24 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
